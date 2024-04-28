a794ae012d2a1cfa7d97d915adec66f9aeeb58b6
josecastroo <castrooj02@gmail.com>
2024-04-28 00:55:55 -0600

    Finish the base project
    The grid was improve and some styles were added, in absence of the additional challenges.

--- main.js
+++ main.js
@@ -1,12 +1,48 @@
 const gridContainer = document.querySelector("#divs-container");
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement("div");
-    div.classList.add("item");
+
+for (let i = 0; i < 16 * 16; i++) {
+    var div = document.createElement("div");
+    //div.classList.add("item");
+
+    div.style.height = "calc(500px / 16)";
+    div.style.width = "calc(500px / 16)";
+    div.style.border = "1px solid black";
+    div.style.boxSizing = "border-box";
+
+    div.addEventListener("mouseover", function() {
+        this.style.backgroundColor = "black";
+    });
+
     gridContainer.appendChild(div);
 }
 
 gridContainer.style.display = "flex";
 gridContainer.style.flexWrap = "wrap";
-gridContainer.style.height = "400px";
-gridContainer.style.width = "400px";
+gridContainer.style.height = "500px";
+gridContainer.style.width = "500px";
 
+const updateButton = document.querySelector("#update-button");
+updateButton.addEventListener("click", updateGridValue);
+
+function updateGridValue () {
+    var size = document.querySelector("#grid-size-input").value;
+    
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    for (let i = 0; i < size * size; i++) {
+        var div = document.createElement("div");
+
+        div.style.height = `calc(500px / ${size})`;
+        div.style.width = `calc(500px / ${size})`;
+        div.style.border = "1px solid black";
+        div.style.boxSizing = "border-box";
+
+        div.addEventListener("mouseover", function() {
+            this.style.backgroundColor = "black";
+        });
+
+        gridContainer.appendChild(div);
+    }
+}
